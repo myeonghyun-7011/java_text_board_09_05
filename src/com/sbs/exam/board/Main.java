59f8c5280dfd7f360f9cc00af279edae540d969f
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-09-05 18:11:37 +0900

    새 게시물 입력받기 기능 구현 시작

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -9,18 +9,32 @@ public class Main {
 
     System.out.println("== 게시판 v 0.1 == ");
     System.out.println("== 프로그램 시작== ");
-    while (true){
+    while (true) {
       System.out.printf("명령 : ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("exit")){
+      if (cmd.equals("exit")) {
         break;
       }
+      else if (cmd.equals("/usr/article/write")) {
+        System.out.println("==게시물 등록 ==");
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String content = sc.nextLine();
+        for (int id = 1; id <= 3; id++) {
+          System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+        }
+      }
+      else {
+        System.out.printf("입력된 명령어 : %s \n", cmd);
+      }
 
-      System.out.printf("입력된 명령어 : %s \n",cmd);
+
+
     }
     System.out.println("== 프로그램 종료 == ");
-
     sc.close();
   }
+
 }
