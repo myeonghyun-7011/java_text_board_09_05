1e96bba98431d43ea62287b9dc269aef592f0618
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-09-07 17:47:05 +0900

    게시물 등록시 새로 생성된 게시물을 article에 추가

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -19,11 +19,15 @@ public class Main {
 
     int articlesLastId = 0;
     Article lastArticle = null; //
-    List<Article> articles = new ArrayList<>(); // Article 전용 리스트 생성
+    List<Article> articles = new ArrayList<>();// Article 전용 리스트 생성
+    // 위에 테스트 게시물을 리스트로 불러오기 위해서 사용.
 
     // 텍스트 게시판 리팩토리하는 방법
     makeTestData(articles);
+   //  자바_텍스트_게시판_만들기();
+    //위에 변수를 (ctrl + 1)을 사용하여 만들게 되면 public하고 생김 남들이 봤을 때, 더 한눈에 알아보기 위해서 사용
 
+    // test게시물을 임의로 3개만 만들어 뒀기 때문에 다음 번호인 4번부터 받아와서 불러오기.
     if(articles.size() > 0){
       articlesLastId = articles.get(articles.size() - 1).id;
     }
@@ -46,11 +50,18 @@ public class Main {
         System.out.printf("내용 : ");
         String content = sc.nextLine();
 
+        // 게시물 번호 하나씩 증가시키기
         int id = articlesLastId + 1;
         articlesLastId = id;
 
+        // 게시물 입력한내용들이 new 객체내용대로  aticle(변수)로 들어옴.
         Article article = new Article(id, title, content);
         lastArticle = article;
+        //new 객체에 내용이 없으면 lastArticle은 null 아무것도 없기 대문에 존재하지 않는다고 뜸.
+
+        //write 에 게시물 등록시 detail 새로 생성된 게시물을 article에 추가.
+        //그래서 4번째 등록 게시물 생성.
+        articles.add(article);
 
 
         System.out.println(("생성된 게시물 객체\n" + article));
@@ -58,9 +69,10 @@ public class Main {
 
       } else if (cmd.equals("/usr/article/detail")) {
 
+        // /usr/article/detail 입력 햇을시 내용물이 없으면 출력하고 다시 명령 하기.
         if (lastArticle == null) {
           System.out.println("게시물이 존재하지 않습니다.");
-          continue;
+          continue; // 위로 다시 돌려보내기.
         }
 
         Article article = lastArticle;
@@ -70,7 +82,8 @@ public class Main {
         System.out.printf("제목 : %s\n", article.title);
         System.out.printf("내용 : %s\n", article.content);
 
-      } else if (cmd.equals("/usr/article/list")) {
+      }
+      else if (cmd.equals("/usr/article/list")) {
         System.out.println("== 게시물 리스트 ==");
         System.out.println("-------------------");
         System.out.println("번호 / 제목");
@@ -95,7 +108,8 @@ public class Main {
           System.out.printf("%d / %s \n", article.id, article.title);
         }
 
-      } else {
+      }
+      else {
         System.out.println("잘못된 명령어 입니다");
       }
     }
